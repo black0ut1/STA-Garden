c9b4e932102d5d86fe1ea12ffb95b3b385dee85d
Petr Pernička <petrpernicka2015@seznam.cz>
2024-12-04 20:28:55 +0100

    improve the graph visualization of AssignmentPanel
    zoom and drag supported

--- src/main/java/black0ut1/gui/AssignmentPanel.java
+++ src/main/java/black0ut1/gui/AssignmentPanel.java
@@ -4,24 +4,77 @@ import black0ut1.data.Network;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import java.awt.event.*;
+import java.awt.geom.Point2D;
 
 public class AssignmentPanel extends JPanel {
 	
 	private static final int NODE_RADIUS = 7;
-	
-	/** Number of pixels from each side */
-	private static final int PADDING = 20;
+	private static final int WHEEL_ROTATION_SCALE_FACTOR = 50;
 	
 	private final Network network;
 	private final Network.Node[] normalizedNodes;
+	private final Network.Node[] scaledNodes;
+	
+	private double scale = 500;
+	private final Point2D.Double offset = new Point2D.Double(0, 0);
+	
+	private Point2D.Double tmpOffset = null;
+	private Point dragStartPos = null;
 	
 	public AssignmentPanel(Network network) {
 		this.normalizedNodes = new Network.Node[network.nodes];
+		this.scaledNodes = new Network.Node[network.nodes];
 		this.network = network;
 		assert network.getNodes() != null;
 		
+		setPreferredSize(new Dimension(600, 600));
+		setSize(600, 600);
+		
+		computeNormalizedNodes();
+		recomputeNodes();
+		
+		addMouseWheelListener(event -> {
+			int rot = event.getWheelRotation();
+			scale -= WHEEL_ROTATION_SCALE_FACTOR * rot;
+			if (scale < 0)
+				scale = 0;
+			
+			recomputeNodes();
+			repaint();
+		});
+		addMouseMotionListener(new MouseAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				// offset must be scaled down and rescaled in recomputeNodes
+				// because only then the zoom will work with respect to middle
+				// of the window (and not w.r.t. the coordinates origin)
+				double dX = (e.getX() - dragStartPos.x) / scale;
+				double dY = (e.getY() - dragStartPos.y) / scale;
+				
+				offset.setLocation(tmpOffset.x + dX, tmpOffset.y + dY);
+				
+				recomputeNodes();
+				repaint();
+			}
+		});
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				tmpOffset = (Point2D.Double) offset.clone();
+				dragStartPos = e.getPoint();
+			}
+		});
+		addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent e) {
+				recomputeNodes();
+				repaint();
+			}
+		});
+	}
+	
+	private void computeNormalizedNodes() {
 		double minX = Double.POSITIVE_INFINITY;
 		double minY = Double.POSITIVE_INFINITY;
 		double maxX = Double.NEGATIVE_INFINITY;
@@ -32,33 +85,28 @@ public class AssignmentPanel extends JPanel {
 			maxX = Math.max(maxX, node.x());
 			maxY = Math.max(maxY, node.y());
 		}
-		double finalMinX = minX;
-		double finalMinY = minY;
 		double lenX = maxX - minX;
 		double lenY = maxY - minY;
+		double midX = (maxX + minX) / 2;
+		double midY = (maxY + minY) / 2;
 		
-		addComponentListener(new ComponentAdapter() {
-			@Override
-			public void componentResized(ComponentEvent e) {
-				for (int i = 0; i < network.nodes; i++) {
-					Network.Node node = network.getNodes()[i];
-					
-					double normX = (node.x() - finalMinX) / lenX;
-					double normY = (node.y() - finalMinY) / lenY;
-					
-					double width = getWidth() - 2 * PADDING;
-					double height = getHeight() - 2 * PADDING;
-					double scaledX = PADDING + Math.round(normX * width);
-					double scaledY = PADDING + Math.round(normY * height);
-					
-					normalizedNodes[i] = new Network.Node(i, scaledX, scaledY);
-				}
-			}
-		});
-		setPreferredSize(new Dimension(600, 600));
-		setSize(600, 600);
+		for (int i = 0; i < network.nodes; i++) {
+			Network.Node node = network.getNodes()[i];
+			double normX = (node.x() - midX) / lenX;
+			double normY = (node.y() - midY) / lenY;
+			normalizedNodes[i] = new Network.Node(i, normX, normY);
+		}
 	}
 	
+	private void recomputeNodes() {
+		for (int i = 0; i < network.nodes; i++) {
+			Network.Node normalizedNode = normalizedNodes[i];
+			double scaledX = (normalizedNode.x() + offset.x) * scale + getWidth() / 2.0;
+			double scaledY = (normalizedNode.y() + offset.y) * scale + getHeight() / 2.0;
+			scaledNodes[i] = new Network.Node(i, scaledX, scaledY);
+		}
+	}
+
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
@@ -77,8 +125,8 @@ public class AssignmentPanel extends JPanel {
 		g.setColor(Color.BLACK);
 		
 		for (Network.Edge edge : network.getEdges()) {
-			Network.Node startNode = normalizedNodes[edge.startNode];
-			Network.Node endNode = normalizedNodes[edge.endNode];
+			Network.Node startNode = scaledNodes[edge.startNode];
+			Network.Node endNode = scaledNodes[edge.endNode];
 			
 			g.drawLine((int) startNode.x(), (int) startNode.y(),
 					(int) endNode.x(), (int) endNode.y());
@@ -88,7 +136,7 @@ public class AssignmentPanel extends JPanel {
 	private void drawNodes(Graphics2D g) {
 		g.setColor(Color.BLACK);
 		
-		for (Network.Node node : normalizedNodes) {
+		for (Network.Node node : scaledNodes) {
 			g.fillOval((int) node.x() - NODE_RADIUS, (int) node.y() - NODE_RADIUS,
 					2 * NODE_RADIUS, 2 * NODE_RADIUS);
 		}
