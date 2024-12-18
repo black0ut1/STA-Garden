7f110ee13fea12f46f57ae40e3ba434f6461414e
Petr Pernička <petrpernicka2015@seznam.cz>
2024-12-18 11:32:53 +0100

    Slight change in checkFlows(), add procedures to remove cyclic flows and to calculate paths from bushes

--- src/main/java/black0ut1/Main.java
+++ src/main/java/black0ut1/Main.java
@@ -3,39 +3,67 @@ package black0ut1;
 import black0ut1.data.DoubleMatrix;
 import black0ut1.data.network.Network;
 import black0ut1.data.tuple.Pair;
+import black0ut1.gui.CriterionChartPanel;
+import black0ut1.gui.GUI;
 import black0ut1.io.TNTP;
 import black0ut1.sta.assignment.Algorithm;
-import black0ut1.sta.assignment.bush.iTAPAS;
+import black0ut1.sta.assignment.link.*;
 import black0ut1.sta.Convergence;
 import black0ut1.sta.cost.*;
 
 public class Main {
 	
 	public static void main(String[] argv) {
-		String map = "Sydney";
+		String map = "SiouxFalls";
 		String networkFile = "data/" + map + "/" + map + "_net.tntp";
 		String odmFile = "data/" + map + "/" + map + "_trips.tntp";
 		String nodeFile = "data/" + map + "/" + map + "_node.tntp";
+		String flowsFile = "data/" + map + "/" + map + "_flow.tntp";
 		
+		// load network and OD matrix
 		var pair = loadData(networkFile, odmFile, nodeFile);
 		Network network = pair.first();
 		DoubleMatrix odMatrix = pair.second();
 		
-		Algorithm.Parameters parameters = new Algorithm.Parameters(
-				network, odMatrix, new BPR(), 50, new Convergence.Builder()
+		// prepare chart for plotting relative gap
+		CriterionChartPanel relativeGapChart = new CriterionChartPanel("Relative gap");
+		new GUI(relativeGapChart);
+		
+		// common convergence criteria for both algorithms
+		Convergence.Builder builder = new Convergence.Builder()
 				.addCriterion(Convergence.Criterion.BECKMANN_FUNCTION)
-				.addCriterion(Convergence.Criterion.RELATIVE_GAP_1, 1e-10));
+				.addCriterion(Convergence.Criterion.RELATIVE_GAP_1, 0.001);
 		
+		// parameters are same apart from updating the chart
+		Algorithm.Parameters parametersFW = new Algorithm.Parameters(
+				network, odMatrix, new BPR(), 50, builder
+				.setCallback(iterationData -> {
+					int i = Convergence.Criterion.RELATIVE_GAP_1.ordinal();
+					relativeGapChart.addValue(iterationData[i], "Frank-Wolfe");
+				}));
 		
-//		Iteration 8
-//		Beckmann function: 9743292,109383011000000
-//		Relative gap 1: 0,000000000005369
-//		-----------------------------------
-		Algorithm fw = new iTAPAS(parameters);
+		Algorithm.Parameters parametersCFW = new Algorithm.Parameters(
+				network, odMatrix, new BPR(), 50, builder
+				.setCallback(iterationData -> {
+					int i = Convergence.Criterion.RELATIVE_GAP_1.ordinal();
+					relativeGapChart.addValue(iterationData[i], "Conjugate Frank-Wolfe");
+				}));
+		
+		// execute both algorithms
+		Algorithm fw = new FrankWolfe(parametersFW);
 		long startTime = System.currentTimeMillis();
 		fw.run();
 		long endTime = System.currentTimeMillis();
 		System.out.println("Static traffic assigment computation time is " + (endTime - startTime) + " ms.");
+		
+		Algorithm cfw = new ConjugateFrankWolfe(parametersCFW);
+		startTime = System.currentTimeMillis();
+		cfw.run();
+		endTime = System.currentTimeMillis();
+		System.out.println("Static traffic assigment computation time is " + (endTime - startTime) + " ms.");
+		
+		// write assigned flows to file
+		TNTP.writeFlows(flowsFile, network, fw.getFlows(), fw.getCosts());
 	}
 	
 	private static Pair<Network, DoubleMatrix> loadData(String networkFile, String odmFile, String nodeFile) {
