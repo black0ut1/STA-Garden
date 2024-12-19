86c979f29b333709051de7387d6afa977e04271f
Petr Pernička <petrpernicka2015@seznam.cz>
2024-12-19 15:24:32 +0100

    Implement linear BPR cost function, implement class for converting UE algorithms to SO algorithms

---
+++ src/main/java/black0ut1/sta/cost/LinearBPR.java
@@ -0,0 +1,21 @@
+package black0ut1.sta.cost;
+
+import black0ut1.data.network.Network;
+
+public class LinearBPR implements CostFunction {
+	
+	@Override
+	public double function(Network.Edge edge, double flow) {
+		return edge.freeFlow * (1 + edge.alpha * flow / edge.capacity);
+	}
+	
+	@Override
+	public double derivative(Network.Edge edge, double flow) {
+		return edge.freeFlow * edge.alpha / edge.capacity;
+	}
+	
+	@Override
+	public double integral(Network.Edge edge, double flow) {
+		return edge.freeFlow * flow * (1 + edge.alpha * flow / (edge.capacity * 2));
+	}
+}
---
+++ src/main/java/black0ut1/sta/cost/SystemOptimumConverter.java
@@ -0,0 +1,38 @@
+package black0ut1.sta.cost;
+
+import black0ut1.data.network.Network;
+
+public class SystemOptimumConverter implements CostFunction {
+	
+	private final CostFunction original;
+	
+	public SystemOptimumConverter(CostFunction original) {
+		this.original = original;
+	}
+	
+	@Override
+	public double function(Network.Edge edge, double flow) {
+		double functionValue = original.function(edge, flow);
+		double derivativeValue = original.derivative(edge, flow);
+		return functionValue + derivativeValue * flow;
+	}
+	
+	@Override
+	public double derivative(Network.Edge edge, double flow) {
+		final double h = 1e-5;
+		double derivativeValue = original.derivative(edge, flow);
+		
+		double derivativeValue1 = original.derivative(edge, flow + h);
+		double derivativeValue2 = original.derivative(edge, flow - h);
+		double secondDerivativeValue = (derivativeValue1 - derivativeValue2) / (2 * h);
+		
+		return 2 * derivativeValue + secondDerivativeValue * flow;
+	}
+	
+	@Override
+	public double integral(Network.Edge edge, double flow) {
+		double functionValue = original.function(edge, flow);
+		double integralValue = original.integral(edge, flow);
+		return functionValue * flow + integralValue;
+	}
+}
