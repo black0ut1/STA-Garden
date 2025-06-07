a4750e292e7d2ae71aff46c66a318ff618732fef
Petr Pernička <petrpernicka2015@seznam.cz>
2025-06-07 11:04:35 +0200

    Refactoring of TampereUnsignalized

--- src/main/java/black0ut1/dynamic/loading/node/TampereUnsignalized.java
+++ src/main/java/black0ut1/dynamic/loading/node/TampereUnsignalized.java
@@ -17,6 +17,7 @@ public class TampereUnsignalized extends Intersection {
 		super(index, incomingLinks, outgoingLinks);
 	}
 	
+	@Override
 	protected DoubleMatrix computeOrientedFlows(DoubleMatrix turningFractions) {
 		DoubleMatrix orientedFlows = new DoubleMatrix(incomingLinks.length, outgoingLinks.length);
 		
@@ -30,7 +31,7 @@ public class TampereUnsignalized extends Intersection {
 			for (int j = 0; j < outgoingLinks.length; j++)
 				orientedSendingFlow.set(i, j, turningFractions.get(i, j) * incomingLinks[i].getSendingFlow());
 		
-		// initialize receiving flow of each outgoing link
+		// initialize reduced receiving flow of each outgoing link
 		double[] R = new double[outgoingLinks.length];
 		for (int j = 0; j < outgoingLinks.length; j++)
 			R[j] = outgoingLinks[j].getReceivingFlow();
@@ -66,11 +67,11 @@ public class TampereUnsignalized extends Intersection {
 			}
 		
 		
-		double[] a = new double[outgoingLinks.length]; // level of reduction
+		double[] LOR = new double[outgoingLinks.length]; // level of reduction
 		while (!J.isClear()) {
 			
 			// 3. Determine most restrictive constraint
-			double minA = Double.POSITIVE_INFINITY;
+			double minLOR = Double.POSITIVE_INFINITY;
 			int minJ = -1;
 			
 			for (int j = 0; j < J.size; j++) {
@@ -81,10 +82,10 @@ public class TampereUnsignalized extends Intersection {
 						if (U[j].get(i))
 							denominator += orientedCapacities.get(i, j);
 					}
-					a[j] = R[j] / denominator;
+					LOR[j] = R[j] / denominator;
 					
-					if (a[j] <= minA) {
-						minA = a[j];
+					if (LOR[j] <= minLOR) {
+						minLOR = LOR[j];
 						minJ = j;
 					}
 				}
@@ -92,69 +93,58 @@ public class TampereUnsignalized extends Intersection {
 			
 			// 4. Determine flows of corresponding set U[minJ] and
 			// recalculate Rj
-			boolean exists = false;
-			for (int i = 0; i < U[minJ].size; i++)
-				if (U[minJ].get(i)) {
-					
-					if (incomingLinks[i].getSendingFlow() <= minA * incomingLinks[i].capacity) {
-						exists = true;
-						break;
-					}
-				}
 			
-			// (a) at least one i in U[minJ] is constrained
-			if (exists) {
-				
-				for (int i = 0; i < U[minJ].size; i++)
-					if (U[minJ].get(i)) {
+			// (a) at least one i in U[minJ] is sending flow (demand)
+			// constrained
+			boolean anyDemandConstrained = false;
+			for (int i = 0; i < U[minJ].size; i++)
+				if (U[minJ].get(i))
+					if (incomingLinks[i].getSendingFlow() <= minLOR * incomingLinks[i].capacity) {
+						anyDemandConstrained = true;
 						
-						if (incomingLinks[i].getSendingFlow() <= minA * incomingLinks[i].capacity) {
-							
-							for (int j = 0; j < outgoingLinks.length; j++)
-								orientedFlows.set(i, j,
-										orientedSendingFlow.get(i, j));
-							
-							for (int j = 0; j < J.size; j++)
-								if (J.get(j)) {
-									R[j] -= orientedSendingFlow.get(i, j);
-									U[j].clear(i);
-									
-									if (U[j].isClear()) {
-										a[j] = 1;
-										J.clear(j);
-									}
+						for (int j = 0; j < outgoingLinks.length; j++)
+							orientedFlows.set(i, j,
+									orientedSendingFlow.get(i, j));
+						
+						for (int j = 0; j < J.size; j++)
+							if (J.get(j)) {
+								R[j] -= orientedSendingFlow.get(i, j);
+								U[j].clear(i);
+								
+								if (U[j].isClear()) {
+									LOR[j] = 1;
+									J.clear(j);
 								}
-						}
+							}
 					}
-				
-			} else {
-				
+			
+			// (b) all links of U[minJ] are constrained by receiving
+			// flow (supply) of link minJ
+			if (!anyDemandConstrained)
 				for (int i = 0; i < U[minJ].size; i++)
 					if (U[minJ].get(i)) {
 						
 						for (int j = 0; j < outgoingLinks.length; j++)
-							orientedFlows.set(i, j,
-									minA * orientedCapacities.get(i, j));
+							orientedFlows.set(i, j, minLOR * orientedCapacities.get(i, j));
 						
 						for (int j = 0; j < J.size; j++)
 							if (J.get(j)) {
 								
-								R[j] -= minA * orientedCapacities.get(i, j);
+								R[j] -= minLOR * orientedCapacities.get(i, j);
 								
 								if (j != minJ) {
 									U[j].clearAll(U[minJ]);
 									
 									if (U[j].isClear()) {
-										a[j] = 1;
+										LOR[j] = 1;
 										J.clear(j);
 									}
 								} else {
-									a[j] = minA;
+									LOR[j] = minLOR;
 									J.clear(j);
 								}
 							}
 					}
-			}
 		}
 		
 		return orientedFlows;
