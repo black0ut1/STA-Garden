f66772ffcf6bd63099b9b17d11d50a3926ad45a8
Petr Pernička <petrpernicka2015@seznam.cz>
2025-07-14 14:03:13 +0200

    Changes in CriterionChartPanel

--- src/main/java/black0ut1/gui/CriterionChartPanel.java
+++ src/main/java/black0ut1/gui/CriterionChartPanel.java
@@ -1,51 +1,54 @@
 package black0ut1.gui;
 
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
+import org.jfree.chart.*;
 import org.jfree.chart.axis.*;
-import org.jfree.chart.plot.CategoryPlot;
-import org.jfree.chart.renderer.category.LineAndShapeRenderer;
-import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.chart.plot.*;
+import org.jfree.chart.renderer.xy.*;
+import org.jfree.data.xy.*;
 
 import javax.swing.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class CriterionChartPanel extends JPanel {
 	
 	private double minvalue = 1e-4;
 	
-	private final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+	private final Map<String, XYSeries> seriesMap = new HashMap<>();
 	private final Map<String, Integer> iterations = new HashMap<>();
 	
 	private final LogAxis yAxis;
+	private final XYSeriesCollection dataset;
 	
 	public CriterionChartPanel(String title) {
-		CategoryAxis xAxis = new CategoryAxis("Iterations");
-		this.yAxis = new LogAxis("Value");
+		dataset = new XYSeriesCollection();
+		
+		NumberAxis xAxis = new NumberAxis("Iterations");
+		yAxis = new LogAxis("Value of convergence criterion");
 		
 		yAxis.setAutoRange(false);
 		yAxis.setTickUnit(new NumberTickUnit(1));
 		yAxis.setRange(minvalue, 10);
 		
-		for (int i = 0; i < 10; i++)
-			dataset.addValue(0, "", Integer.valueOf(i));
+		XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer(true, true);
 		
-		LineAndShapeRenderer renderer = new LineAndShapeRenderer(true, true);
-		CategoryPlot plot = new CategoryPlot(dataset, xAxis, yAxis, renderer);
-		JFreeChart chart = new JFreeChart(title, plot);
+		XYPlot plot = new XYPlot(dataset, xAxis, yAxis, renderer);
+		JFreeChart chart = new JFreeChart(title, JFreeChart.DEFAULT_TITLE_FONT, plot, true);
 		
 		add(new ChartPanel(chart));
 	}
 	
 	public void addValue(double value, String algorithmName) {
-		if (!iterations.containsKey(algorithmName))
-			iterations.put(algorithmName, 0);
+		XYSeries series = seriesMap.computeIfAbsent(algorithmName, name -> {
+			XYSeries newSeries = new XYSeries(name);
+			dataset.addSeries(newSeries);
+			return newSeries;
+		});
 		
-		Integer i = iterations.get(algorithmName);
-		dataset.addValue(value, algorithmName, i);
-		iterations.put(algorithmName, i + 1);
+		int iteration = iterations.getOrDefault(algorithmName, 0);
+		series.add(iteration, value);
+		iterations.put(algorithmName, iteration + 1);
 		
+		// Update Y-axis range
 		minvalue = Math.min(minvalue, value);
 		double logFloor = Math.pow(10, Math.floor(Math.log10(minvalue)));
 		yAxis.setRange(logFloor, 10);
