30ecc2116e2a8c21de7f32c47c53e8ff7587cb28
Petr Pernička <petrpernicka2015@seznam.cz>
2026-03-10 13:34:21 +0100

    Optimize TimeDependentODM a little

--- src/main/java/black0ut1/dynamic/TimeDependentODM.java
+++ src/main/java/black0ut1/dynamic/TimeDependentODM.java
@@ -2,25 +2,21 @@ package black0ut1.dynamic;
 
 import black0ut1.data.DoubleMatrix;
 
-import java.util.Arrays;
-
 /**
- * Time-dependent OD matrix, where each OD totalFlow is represented
- * by a time sequence.
- * TODO implement as 1D array and analyze access patterns
+ * Time-dependent OD matrix, where each OD entry is a dicrete-time sequence of flows.
  */
 public class TimeDependentODM {
 	
 	/** 3D array, where first index is origin, second is destination
 	 *  and third is the time interval. */
-	private final double[][][] flow;
+	protected final double[] flow;
 	
 	public final int zones;
 	public final int timeSteps;
 	
-	public TimeDependentODM(double[][][] flow, int timeSteps) {
+	protected TimeDependentODM(double[] flow, int zones, int timeSteps) {
 		this.flow = flow;
-		this.zones = flow.length;
+		this.zones = zones;
 		this.timeSteps = timeSteps;
 	}
 	
@@ -28,19 +24,20 @@ public class TimeDependentODM {
 		if (time >= timeSteps)
 			return 0;
 		
-		return flow[origin][destination][time];
+		return flow[time * zones * zones + origin * zones + destination];
 	}
 	
 	public static TimeDependentODM fromStaticODM(DoubleMatrix odm, int timeSteps) {
-		double[][][] flow = new double[odm.n][odm.n][timeSteps];
+		double[] flow = new double[odm.n * odm.n * timeSteps];
 		
-		for (int i = 0; i < odm.n; i++) {
+		for (int i = 0; i < odm.n; i++)
 			for (int j = 0; j < odm.n; j++) {
 				double uniformFlow = odm.get(i, j) / timeSteps;
-				Arrays.fill(flow[i][j], uniformFlow);
+				
+				for (int t = 0; t < timeSteps; t++)
+					flow[t * odm.n * odm.n + i * odm.n + j] = uniformFlow;
 			}
-		}
 		
-		return new TimeDependentODM(flow, timeSteps);
+		return new TimeDependentODM(flow, odm.n, timeSteps);
 	}
 }
